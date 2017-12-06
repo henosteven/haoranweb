60e410d9f9a22aba8ad933602465ba3db90fe5c5
henosteven <henosteven@126.com>
2017-12-06 19:20:38 +0800

    演示注册controller

--- example/demo.go
+++ example/demo.go
@@ -4,20 +4,39 @@ import (
     "henoweb"
     "syscall"
     "fmt"
+    "net/http"
+    "os"
 )
 
 func main() {
+    /* 注册信号处理函数 */
     henoweb.RegisterSignalHook(syscall.SIGINT, recvSigINT)
     henoweb.RegisterSignalHook(syscall.SIGTERM, recvSigTERM)
+    
+    /* 注册路由 */
+    henoweb.RegisterFuncRouter("/example", example)
+
+    indexController := IndexController{}
+    henoweb.RegisterControllerRouter("/example2", indexController)
     henoweb.Run()
 }
 
 func recvSigINT() error {
     fmt.Println("recv sigint ~hi~")
+    os.Exit(1)
     return nil
 }
 
 func recvSigTERM() error{
     fmt.Println("recv sigterm ~hi~")
+    os.Exit(1)
     return nil
+}
+
+func example(w http.ResponseWriter, r *http.Request) {
+    fmt.Fprintf(w, "hello~example~")
+}
+
+type IndexController struct {
+    henoweb.HENOController
 }
