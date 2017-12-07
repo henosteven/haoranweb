24aa6fc1a4467fca70bd74fbe5169a0fbe68cdfc
henosteven <henosteven@126.com>
2017-12-07 16:47:42 +0800

    增加匹配类型

--- henoweb/henorouter.go
+++ henoweb/henorouter.go
@@ -2,6 +2,7 @@ package henoweb
 
 import (
     "net/http"
+    "strings"
     "fmt"
 )
 
@@ -22,21 +23,52 @@ func RegisterFuncRouter(pattern string, h http.HandlerFunc) {
 }
 
 func RegisterControllerRouter(pattern string, c Controller) {
-    hRouter.HandleMap[pattern] = c
+    hRouter.HandleMap[strings.TrimLeft(pattern, "/")] = c
 }
 
 func (router *HenoRouter) ServeHTTP(w http.ResponseWriter, r *http.Request) {
     h, ok := router.HandleMap[r.URL.Path]
+    var action, controller string
+    action, controller = getControllerAction(r.URL.Path)
     if !ok {
-        w.WriteHeader(http.StatusNotFound)
-        fmt.Fprintln(w, "~opps 404~")
-    } else {
-        switch h.(type) {
-            case http.HandlerFunc:
-                h.(http.HandlerFunc).ServeHTTP(w, r) 
-            case Controller:
-                fmt.Println(r.URL)
-                Invoke(h, "Index", w, r)
+        h, ok = router.HandleMap[controller]
+        if !ok {
+            w.WriteHeader(http.StatusNotFound)
+            fmt.Fprintln(w, "~opps 404~")
+            return
         }
     }
+
+    switch h.(type) {
+        case http.HandlerFunc:
+            h.(http.HandlerFunc).ServeHTTP(w, r) 
+        case Controller:
+            action = Ucfirst(action)
+            Invoke(h, action, w, r)
+    }
+}
+
+func getControllerAction(path string) (action, controller string) {
+    pathItem := strings.Split(strings.Trim(path, "/"), "/")
+    fmt.Println(pathItem)
+    controller = "index"
+    action = "index"
+    if len(pathItem) >= 1 {
+        controller = pathItem[0]
+    }
+    if len(pathItem) > 1 {
+      action = pathItem[1]
+    }
+    return
+}
+
+func Ucfirst(str string) string {
+   if str == "" {
+       return str
+   }
+   b := []byte(str)
+   if b[0] >=97 && b[0] <= 122 {
+       b[0] = b[0] - 32
+   } 
+   return string(b)
 }
