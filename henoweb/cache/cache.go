61a921b4f6727c1ce8adcd561a4add1e58bfd975
henosteven <henosteven@126.com>
2017-12-11 14:20:33 +0800

    增加hset hget hgetall

--- henoweb/cache/cache.go
+++ henoweb/cache/cache.go
@@ -24,10 +24,44 @@ func Set(k, v string) {
 }
 
 func Get(k string) string{
-    var err error
     v, err := redis.String(RedisConn.Do("GET", k))
     if err != nil {
         fmt.Println(err)
     }
     return v
+}
+
+func Hset(k, field, v string) {
+    RedisConn.Do("HSET", k, field, v)
+}
+
+func Hget(k, field string) string{
+    v, err := RedisConn.Do("HGET", k, field)
+    if err != nil {
+        fmt.Println(err)
+    }
+    result, _ := v.([]byte)
+    return string(result)
+}
+
+func Hgetall(k string) map[string]string{
+    v, err := RedisConn.Do("HGETALL", k)
+    if err != nil {
+        fmt.Println(err)
+    }
+    result, _ := v.([]interface{})
+    var stringlist  = make(map[string]string, 0)
+    if len(result) > 0 {
+        var curKey string = ""
+        for _, tmp := range result {
+            tmpp, _ := tmp.([]byte)
+            if curKey == "" {
+                curKey = string(tmpp)
+            } else {
+                stringlist[curKey] = string(tmpp)
+                curKey = ""
+            }
+        }
+    } 
+    return stringlist
 }
--- henoweb/cache/cache_test.go
+++ henoweb/cache/cache_test.go
@@ -7,6 +7,14 @@ import (
 func TestGet(t *testing.T) {
     InitRedisServer()
     Set("user", "jinjing")
+    Hset("jinjing", "name", "jinjing")
+    Hset("jinjing", "age", "28")
+
+    age := Hget("jinjing", "age")
+    if age != "28" {
+        t.Errorf("expect: %s  , real: %s", "28", age)
+    }
+Hgetall("jinjing")
     user := Get("user")
     if user != "jinjing" {
         t.Errorf("expect: %s  , real: %s", "jinjing", user)
